47a5497bc1eebc00cb6386463f373036623cd478
hamdiabdulaziz <hamdi@intelove.com>
2023-04-10 10:27:42 +0700

    crud dasar django

---
+++ project/students/views.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+from rest_framework.viewsets import ModelViewSet
+from .models import Student
+from .serializers import StudentSerializer
+
+
+class StudentsViewSet(ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
